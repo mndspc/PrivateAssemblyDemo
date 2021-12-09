745ab33cd8e27bb3c555e2314bfc2aac4b4cdaeb
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-12-09 18:01:53 +0530

    Pushing new project
    this contains gst class

---
+++ GstUtilities/GstUtilities/Gst.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GstUtilities
+{
+    public class Gst
+    {
+        public double ProAmt { get; set; }
+
+        public double GstPer { get; set; }
+
+
+        public Gst(double proAmt, double gstPer)
+        {
+            this.ProAmt = proAmt;
+            this.GstPer = gstPer;
+        }
+
+        public double CalculateGstAmt()
+        {
+            double GstAmt = this.ProAmt / 100 * this.GstPer;
+            return GstAmt;
+        }
+
+        public double CalculateTotalProAmt()
+        {
+            double totalAmt = CalculateGstAmt() + this.ProAmt;
+            return totalAmt;
+        }
+    }
+}
